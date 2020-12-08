2c14b08276731ae7b960aac44c4eb4523d676941
Clement Saw Kiat Chun <cskiat06@gmail.com>
2020-12-08 09:46:37 +0800

    Remove greenTeaPaste

--- src/main/java/fivefishes/design/patterns/group/assignment/entities/abstractFactory/ingredient/paste/GreenTeaPaste.java
+++
@@ -1,5 +0,0 @@
-package fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.paste;
-
-public abstract class GreenTeaPaste {
-    String description;
-}
--- src/main/java/fivefishes/design/patterns/group/assignment/interfaces/abstractFactory/MooncakeFactory.java
+++ src/main/java/fivefishes/design/patterns/group/assignment/interfaces/abstractFactory/MooncakeFactory.java
@@ -3,7 +3,6 @@ package fivefishes.design.patterns.group.assignment.interfaces.abstractFactory;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.dough.Dough;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.fillings.Fillings;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.mooncakeImage.MooncakeImage;
-import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.paste.GreenTeaPaste;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.paste.LotusSeedPaste;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.paste.RedBeanPaste;
 import fivefishes.design.patterns.group.assignment.entities.abstractFactory.ingredient.shape.Shape;
@@ -13,7 +12,6 @@ public interface MooncakeFactory {
     public Dough createDough();
     public Shape createShape();
     public RedBeanPaste createRedBeanPaste();
-    public GreenTeaPaste createGreenTeaPaste();
     public LotusSeedPaste createLotusSeedPaste();
     public Fillings createFillings();
     public MooncakeImage createImage();
